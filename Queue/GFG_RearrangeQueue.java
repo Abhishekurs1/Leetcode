3807b8d41238c91127d0ba7986ee517530927178
Abhishek <abhishekurs123456@gmail.com>
2026-04-08 18:24:46 +0530

    updated rearrange

--- Queue/GFG_RearrangeQueue.java
+++ Queue/GFG_RearrangeQueue.java
@@ -12,3 +12,38 @@ class Solution {
         return q;
     }
 }
+
+
+// Another Solution using Stack
+
+class Solution {
+    public Queue<Integer> rearrangeQueue(Queue<Integer> q) {
+        Stack<Integer> st = new Stack<>();
+        int n = q.size();
+        for(int i = 1;i<=n/2;i++){
+           st.push(q.remove()); 
+        }
+        while(st.size()>0){
+            q.add(st.pop());
+        }
+        for(int i = 1;i<=n/2;i++){
+           st.push(q.remove()); 
+        }
+        
+        // Rearrangement
+        while(st.size()>0){
+            q.add(st.pop());
+            q.add(q.remove());
+        }
+        
+        // Reverse
+        while(q.size()>0){
+            st.push(q.remove());
+        }
+        
+        while(st.size()>0){
+            q.add(st.pop());
+        }
+	return q;
+    }
+}
