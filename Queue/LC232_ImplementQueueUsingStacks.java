715ef6ba0a11925947c731de8878a504e1b57fd7
Abhishek <abhishekurs123456@gmail.com>
2026-04-08 17:21:53 +0530

    Added LC_232 Solution

---
+++ Queue/LC232_ImplementQueueUsingStacks.java
@@ -0,0 +1,73 @@
+class MyQueue {
+
+    // Pop Efficient Approach
+    Stack<Integer> st=new Stack<>();
+    Stack<Integer> helper=new Stack<>();
+    public MyQueue() {
+        
+    }
+    
+    public void push(int x) {   //O(n)
+        while(st.size()>0){
+            helper.push(st.pop());
+        }
+        st.push(x);
+        while(helper.size()>0){
+            st.push(helper.pop());
+        }
+    }
+    
+    public int pop() {  //O(1)
+        return st.pop();
+    }
+    
+    public int peek() {  //O(1)
+        return st.peek();
+        
+    }
+    
+    public boolean empty() {
+        return (st.size()==0);
+    }
+}
+
+    // Add Efficient Approach
+
+// class MyQueue {
+//     Stack<Integer> st=new Stack<>();
+//     Stack<Integer> helper=new Stack<>();
+//     public MyQueue() {
+        
+//     }
+    
+//     public void push(int x) {   //O(1)
+//         st.push(x);
+//     }
+    
+//     public int pop() {  //O(n)
+//         while(st.size()>1){
+//             helper.push(st.pop());
+//         }
+//         int front = st.pop();
+//         while(helper.size()>0){
+//             st.push(helper.pop());
+//         }
+//         return front;
+//     }
+    
+//     public int peek() {  //O(n)
+//          while(st.size()>1){
+//             helper.push(st.pop());
+//         }
+//         int front = st.peek();
+//         while(helper.size()>0){
+//             st.push(helper.pop());
+//         }
+//         return front;
+        
+//     }
+    
+//     public boolean empty() {
+//         return (st.size()==0);
+//     }
+// }
